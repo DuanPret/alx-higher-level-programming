5d2f2090b52ca6fe82ad872f8c3dce3ba66ee6f3
Duan <duanpretorius15@gmail.com>
2023-01-04 15:48:14 -0800

    fixed missing files

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -13,6 +14,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
