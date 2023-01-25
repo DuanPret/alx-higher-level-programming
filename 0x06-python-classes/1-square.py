fdcc3ee1f116bd2ed268147e241597379746129e
Duan <duanpretorius15@gmail.com>
2023-01-25 10:24:20 -0800

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines a square based on task 0-square.py
+"""
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size):
+        """ Class initialization """
+        self.__size = size
