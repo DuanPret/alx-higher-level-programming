b0a54bf97ea4b9c735a3bc0e76f45dc28ec615ed
Duan <duanpretorius15@gmail.com>
2023-01-26 09:26:00 -0800

    fixed indentation

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,12 +5,12 @@
 class Square:
     """ Square class """
     def __init__(self, size=0):
-       if type(size) != int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
-       elif size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-       else:
-           self.__size = size
+        else:
+            self.__size = size
 
     @property
     def size(self):
