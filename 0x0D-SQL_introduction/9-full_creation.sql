1be31a238e5edd49d09912c2261897ec808b00ee
Duan <duanpretorius15@gmail.com>
2023-03-15 16:11:01 -0700

    fixed bug in task 4 and 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 -- creates a table second_table
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-SELECT INTO second_table(id, name, score) VALUE(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', score = 8)
+INSERT INTO second_table(id, name, score) VALUE(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', score = 8)
